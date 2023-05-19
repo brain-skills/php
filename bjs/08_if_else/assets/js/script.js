f7eaa690765165c6fb56c0e335e06e6a0c5e95af
smart-skill <web.pr@mail.ru>
2023-05-20 00:37:25 +0400

    init version 0.0.0.2

--- bjs/08_if_else/assets/js/script.js
+++ bjs/08_if_else/assets/js/script.js
@@ -1,9 +1,18 @@
 let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
 let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
-alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
+if (isNaN(minValue) || isNaN(maxValue)){
+    minValue = 0;
+    maxValue = 100;
+}else if (minValue === 'string' || maxValue === 'string'){
+    minValue = 0;
+    maxValue = 100;
+}
 let answerNumber  = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
+(maxValue > 999) ? maxValue = 999 : maxValue;
+(minValue < -999) ? minValue = -999 : minValue;
+alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
 
 const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
@@ -81,35 +90,62 @@ document.getElementById('btnEqual').addEventListener('click', function() {
     }
 });
 
+// Последний вариант
+// const numb900 = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+// const numb90 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+// const numb9 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numb19 = [11, 12, 13, 14, 15, 16, 17, 18, 19];
 
+// const string900 = ['сто', 'двести', 'триста', 'четыреста', 'пятьсот', 'шестьсот', 'семьсот', 'восемьсот', 'девятьсот'];
+// const string90 = ['десять', 'двадцать', 'тридцать', 'сорок', 'пятьдесят', 'шестьдесят', 'семьдесят', 'восемьдесят', 'девяносто'];
+// const string9 = ['один', 'два', 'три', 'четыре', 'пять', 'шесть', 'семь', 'восемь', 'девять'];
+// const string19 = ['одиннацать', 'двенадцать', 'тринадцать', 'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать'];
 
-
-
-
-
-
-
-
-
-
-
-
-
-// Alert swal
-// Swal.fire({
-//     position: 'top-end',
-//     icon: 'success',
-//     title: 'Your work has been saved',
-//     showConfirmButton: false,
-//     timer: 1500
-// });
-
-// Prompt swal
-// const { value: url } = await Swal.fire({
-//     input: 'url',
-//     inputLabel: 'URL address',
-//     inputPlaceholder: 'Enter the URL'
-// })
-// if (url) {
-//  Swal.fire(`Entered URL: ${url}`)
+// function transformation(number){
+//     for(i = 0; i < 9; i++){
+//         if(numb900[i] == number){
+//             return string900[i];
+//         } else if(numb90[i] == number){
+//             return string90[i];
+//         } else if(numb9[i] == number){
+//             return string9[i];
+//         } else if(numb19[i] == number){
+//             return string19[i];
+//         }
+//         console.log(numb900[i]+numb90[i]+numb9[i]+numb19[i].join(""));
+//     }
 // }
+// console.log(transformation(200));
+
+// Вариант с помощью switch ===========================================================================
+// function transformation(number){
+//     switch(number){
+//         case 1: 'один';
+//         breack;
+//         case 2: 'два';
+//         breack;
+//         default: 'три';
+//     }
+// }
+// console.log(transformation(200));
+
+// Вариант с помощью if ==============================================================================
+// if (numb900[0] == number){
+    //     return string900[0]
+    // } else if (numb900[1] == number){
+    //     return string900[1]
+    // } else if (numb900[2] == number){
+    //     return string900[2]
+    // } else if (numb900[3] == number){
+    //     return string900[3]
+    // } else if (numb900[4] == number){
+    //     return string900[4]
+    // } else if (numb900[5] == number){
+    //     return string900[5]
+    // } else if (numb900[6] == number){
+    //     return string900[6]
+    // } else if (numb900[7] == number){
+    //     return string900[7]
+    // } else if (numb900[8] == number){
+    //     return string900[8]
+    // }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Калькулятор - [ссылка](https://smart-skill.github.io/php/bjs/07_Number_and_string/)
+## Угадайка - [ссылка](https://smart-skill.github.io/php/bjs/08_if_else/)
+## function_object - [ссылка](https://smart-skill.github.io/php/bjs/10_function_object/)
